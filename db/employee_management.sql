da842f7ed32771f69718560e11af77c2277f5e09
Brian Toney <btoney1988@gmail.com>
2020-11-02 18:37:20 -0500

    employee table created

--- db/employee_management.sql
+++ db/employee_management.sql
@@ -17,3 +17,12 @@ CREATE TABLE role (
   FOREIGN KEY (department_id) REFERENCES department (id)
 );
 
+CREATE TABLE employee (
+  id INT  AUTO_INCREMENT NOT NULL PRIMARY KEY,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INT NOT NULL,
+  FOREIGN KEY (role_id) REFERENCES role (id),
+  manager_id INT NULL,
+  FOREIGN KEY (manager_id) REFERENCES manager (id) 
+)
