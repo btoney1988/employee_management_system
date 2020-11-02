39ebec54f6fd7ed29008af378b80743ffb58c554
Brian Toney <btoney1988@gmail.com>
2020-11-02 18:34:59 -0500

    Role table complete

--- db/employee_management.sql
+++ db/employee_management.sql
@@ -5,7 +5,15 @@ CREATE DATABASE employee_managementDB;
 USE employee_managementDB;
 
 CREATE TABLE department (
-  id INT AUTO_INCREMENT NOT NULL,
+  id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
   name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE role (
+  id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL NOT NULL,
+  department_id INT NOT NULL,
+  FOREIGN KEY (department_id) REFERENCES department (id)
 );
 
