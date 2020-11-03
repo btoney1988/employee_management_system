7ce320bfa02720bbbd78cd16a7d2a61fee7c4e11
Brian Toney <btoney1988@gmail.com>
2020-11-03 09:10:04 -0500

    Start function with inquirer to route to user correctly

--- server.js
+++ server.js
@@ -15,3 +15,49 @@ connection.connect(function (err) {
   start();
 });
 
+function start() {
+  inquirer
+    .prompt({
+      type: "list",
+      name: "command",
+      message: "Choose below.",
+      choices: [
+        "Add a Department",
+        "Add a Role",
+        "Add an Employees",
+        "View Departments",
+        "View Roles",
+        "View Employees",
+        "Update Roles",
+        "Exit",
+      ]
+    })
+    .then(function (answer) {
+      switch (answer.command) {
+        case "Add a Department":
+          addDepartment();
+          break;
+        case "Add a Role":
+          addRole();
+          break;
+        case "Add an Employee":
+          addEmployee();
+          break;
+        case "View Departments":
+          viewDepartments();
+          break;
+        case "View Roles":
+          viewRoles();
+          break;
+        case "View Employees":
+          viewEmployees();
+          break;
+        case "Update Roles":
+          updateRoles();
+          break;
+        case "Exit":
+          connection.end();
+          break;
+      }
+    });
+};
